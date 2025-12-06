50dab175e00ccdd75b575d4450399b7583210c05
GiaHuy999 <huyphan2792005@gmail.com>
2025-12-06 13:18:21 +0700

    update

--- garage/__init__.py
+++ garage/__init__.py
@@ -5,7 +5,7 @@ from flask_login import LoginManager
 app = Flask(__name__)
 
 app.secret_key = "dwdswdw"
-app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://root:root@localhost/saledb?charset=utf8mb4"
+app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://root:root@localhost/garage?charset=utf8mb4"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = True
 app.config["PAGE_SIZE"] =3
 
